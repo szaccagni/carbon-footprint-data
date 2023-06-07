05884b3464a241595c47c1f27c21dbc1ff4c00b9
szaccagni <sarahzaccagni@gmail.com>
2023-06-07 19:17:44 -0400

    finds specific footprint

--- pages/api/footprints.js
+++ pages/api/footprints.js
@@ -5,8 +5,10 @@ connectDB();
 
 export default async (req, res) => {
   try {
-    const data = await Footprint.find()
-    res.json(data[0])
+    const footprint = await Footprint.find({zipCode: req.query.zip})
+    res.json(footprint)
+    // const data = await Footprint.find()
+    // res.json(data[0])
   } catch (error) {
     console.error('Error retrieving data', error);
     res.status(500).json({ error: 'Error retrieving data' });
