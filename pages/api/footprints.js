249e959f013686515c2788eb930c2f3ab2d31849
szaccagni <sarahzaccagni@gmail.com>
2023-06-07 22:14:23 -0400

    updates cors header

--- pages/api/footprints.js
+++ pages/api/footprints.js
@@ -7,14 +7,17 @@ export default async (req, res) => {
   try {
     const footprint = await Footprint.find({zipCode: req.query.zip})
     if (footprint.length > 0) {
+      res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
       res.json(footprint)
     } else {
+      res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
       res.status(404).json({error: 'zip code not found'})
     }
     // const data = await Footprint.find()
     // res.json(data[0])
   } catch (error) {
     console.error('Error retrieving data', error);
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
     res.status(500).json({ error: 'Error retrieving data' });
   }
 };
