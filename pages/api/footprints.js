7dc50a6aa183479aeaad4b6016be82a77e5ba59b
szaccagni <sarahzaccagni@gmail.com>
2023-06-09 00:35:53 -0400

    updates access control header

--- pages/api/footprints.js
+++ pages/api/footprints.js
@@ -4,20 +4,21 @@ import connectDB from '../../db'
 connectDB();
 
 export default async (req, res) => {
+  const allowedOrigins = ['http://localhost:3000', 'https://carbon-footprint-calc.vercel.app']
+  const origin = req.headers.origin
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader('Access-Control-Allow-Origin', origin);
+  }
+
   try {
     const footprint = await Footprint.find({zipCode: req.query.zip})
     if (footprint.length > 0) {
-      res.setHeader('Access-Control-Allow-Origin', ['http://localhost:3000', 'https://carbon-footprint-calc.vercel.app']);
       res.json(footprint)
     } else {
-      res.setHeader('Access-Control-Allow-Origin', ['http://localhost:3000', 'https://carbon-footprint-calc.vercel.app']);
       res.status(404).json({error: 'zip code not found'})
     }
-    // const data = await Footprint.find()
-    // res.json(data[0])
   } catch (error) {
     console.error('Error retrieving data', error);
-    res.setHeader('Access-Control-Allow-Origin', ['http://localhost:3000', 'https://carbon-footprint-calc.vercel.app']);
     res.status(500).json({ error: 'Error retrieving data' });
   }
 };
